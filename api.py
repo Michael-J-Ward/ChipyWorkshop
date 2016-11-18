875a787869bd0fac5f7638ccc3681546469118a8
Dryden Bouamalay <bouamalayd@gmail.com>
2016-11-17 20:18:38 -0600

    finished

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import meetup.api
 client = meetup.api.Client('c725210249306a162b17454c27384c')
 
 def get_meetup_members(event_id):
+
 	rsvps = client.GetRsvps(event_id=event_id, urlname='_ChiPy_')
 	member_id = ','.join([str(i['member']['member_id']) for i in rsvps.results])
 	members = client.GetMembers(member_id=member_id)
@@ -10,11 +11,15 @@ def get_meetup_members(event_id):
 	print(member_id)
 	print(members)
 
+	results = [] 
+
 	for member in members.results:
 	    try:
-	    	yield member
+	    	r = dict(name=member['name'], id=member['id'], link=member['photo']['thumb_link'])
+	    	results.append(r)
 	        #print('{0},{1},{2}'.format(member['name'], member['id'], member['photo']['thumb_link']))
 	    except:
 	        pass # ignore those who do not have a complete profile
-
+	        
+	return results
 # get_meetup_members('235484841')
--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 from api import get_meetup_members
 
-class MyForm(FlaskForm):
+import pprint 
+
+class MeetupForm(FlaskForm):
     id = IntegerField('id', validators=[DataRequired()])
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    form = MeetupForm()
+    if form.validate_on_submit():
+        event_id = str(form.id.data)
+        return redirect('/meetup/' + event_id)
+    return render_template('index.html', form=form) 
 
-@app.route('/meetup')
-def meetup():
-    # somehow we will get an event id from the form
-    members = list(get_meetup_members('235484841'))
-    return render_template('members.html', members=members)
+@app.route('/meetup/<event_id>')
+def meetup(event_id):
+    # somehow we    will get an event id from the form
+    members = get_meetup_members(event_id)
+    return render_template('test.html', members=members)
 
 if __name__ == '__main__':
     app.run(debug=True)
