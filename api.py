3b872fb4ef9557d111dd98d8651c180889506381
Dryden Bouamalay <bouamalayd@gmail.com>
2016-11-17 19:46:42 -0600

    added stuff

--- api.py
+++ api.py
@@ -1,12 +1,20 @@
 import meetup.api
 client = meetup.api.Client('c725210249306a162b17454c27384c')
 
-rsvps=client.GetRsvps(event_id='235484841', urlname='_ChiPy_')
-member_id = ','.join([str(i['member']['member_id']) for i in rsvps.results])
-members = client.GetMembers(member_id=member_id)
+def get_meetup_members(event_id):
+	rsvps = client.GetRsvps(event_id=event_id, urlname='_ChiPy_')
+	member_id = ','.join([str(i['member']['member_id']) for i in rsvps.results])
+	members = client.GetMembers(member_id=member_id)
 
-for member in members.results:
-    try:
-        print('{0},{1},{2}'.format(member['name'], member['id'], member['photo']['thumb_link']))
-    except:
-        pass # ignore those who do not have a complete profile
+	print(rsvps)
+	print(member_id)
+	print(members)
+
+	for member in members.results:
+	    try:
+	    	yield member
+	        #print('{0},{1},{2}'.format(member['name'], member['id'], member['photo']['thumb_link']))
+	    except:
+	        pass # ignore those who do not have a complete profile
+
+# get_meetup_members('235484841')
--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
+from flask import Flask, render_template
+
+from api import get_meetup_members
 
 class MyForm(FlaskForm):
-    name = IntegerField('name', validators=[DataRequired()])
+    id = IntegerField('id', validators=[DataRequired()])
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/meetup')
+def meetup():
+    # somehow we will get an event id from the form
+    members = list(get_meetup_members('235484841'))
+    return render_template('members.html', members=members)
 
 if __name__ == '__main__':
     app.run(debug=True)
